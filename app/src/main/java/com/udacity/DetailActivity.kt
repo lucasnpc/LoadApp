65fbc7516d894d81f8ddf693545a02eead5db4cb
lucasnpc <lucasnpc2@outlook.com>
2022-07-29 20:06:43 -0300

    Initial commit

---
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -0,0 +1,21 @@
+package com.udacity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.udacity.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private val binding: ActivityDetailBinding by lazy {
+        ActivityDetailBinding.inflate(
+            layoutInflater
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+    }
+
+}
